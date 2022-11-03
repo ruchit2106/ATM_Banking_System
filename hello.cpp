52f95e3eab884a71432be7dc26d3e350824791ec
ruchit2106 <87220033+ruchit2106@users.noreply.github.com>
2022-11-03 17:42:53 +0530

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string a="C:\Users\Ruchit\Github\ATM_BANKING_SYSTEM\IMAGES\hey.txt";
+    std::cout<<a;
+    
+}
