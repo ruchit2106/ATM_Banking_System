e6a341d5063e9362a26fa6a747c3fc4b7623e648
whitewizard2002 <70932466+whitewizard2002@users.noreply.github.com>
2022-11-05 18:30:31 +0530

    Page Routing

---
+++ ATM_Banking_System/Controllers/AdminsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ATM_Banking_System.Controllers
+{
+    public class AdminsController : Controller
+    {
+        public IActionResult AdminDashboard()
+        {
+            return View();
+        }
+
+        public IActionResult ChangeProfile()
+        {
+            return View();
+        }
+
+        public IActionResult ManageUsers()
+        {
+            return View();
+        }
+        public IActionResult ViewATMBalance()
+        {
+            return View();
+        }
+        public IActionResult ViewTransactionHistory()
+        {
+            return View();
+        }
+        public IActionResult DepositMoney()
+        {
+            return View();
+        }
+        public IActionResult WithdrawMoney()
+        {
+            return View();
+        }
+
+    }
+}
